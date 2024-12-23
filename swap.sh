a236a18af65cd94dc5017d104c5f3edd75cd81d6
reshakk <140834027+reshakk@users.noreply.github.com>
2024-12-23 17:49:37 +0300

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#swapon --show 
+
+set -e
+
+#Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root"
+        exit
+fi
+
 fallocate -l 2G /swapfile
 
 chmod 600 /swapfile
@@ -9,14 +19,13 @@ mkswap /swapfile
 swapon /swapfile
 
 FSTAB_FILE="/etc/fstab"
-
 FSTAB_LINE="/swapfile none swap sw 0 0"
 
 if ! grep -Fxq "$FSTAB_LINE" $FSTAB_FILE; then
     echo "$FSTAB_LINE" | sudo tee -a $FSTAB_FILE
-    echo "Add sting in $FSTAB_FILE"
+    echo "Add string in $FSTAB_FILE"
 else
-    echo "The string already exists in $FSTAB_FILE"
+    echo "String is already exist in $FSTAB_FILE"
 fi
 
 SYSCTL_FILE="/etc/sysctl.conf"
@@ -25,11 +34,11 @@ SYSCTL_LINE="vm.swappiness=10"
 
 if ! grep -Fxq "$SYSCTL_LINE" $SYSCTL_FILE; then
     echo "$SYSCTL_LINE" | sudo tee -a $SYSCTL_FILE
-    echo "Add sting in $SYSCTL_FILE"
+    echo "Add string in $SYSCTL_FILE"
 else
-    echo "The string already exists in $SYSCTL_FILE"
+    echo "String is already exist in $SYSCTL_FILE"
 fi
 
-sudo sysctl -p
+sysctl -p
 
-echo "Successful install."
+echo "All changes have been accepted."
