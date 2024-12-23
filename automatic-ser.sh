c1b704e298a560df95c98e05affe2ddad5bd8d6f
reshakk <140834027+reshakk@users.noreply.github.com>
2024-12-23 17:57:51 +0300

    Update automatic-ser.sh

--- automatic-ser.sh
+++ automatic-ser.sh
@@ -10,22 +10,15 @@
 #  ░░   ░    ░   ░  ░  ░   ░  ░░ ░  ░   ▒   ░ ░░ ░ ░ ░░ ░ 
 #   ░        ░  ░      ░   ░  ░  ░      ░  ░░  ░   ░  ░   
 
+set -e
 
-# Define color codes
-
-ERROR='\033[0;31m'
-READ='\033[0;32m'
-OUTPUT='\033[0;33m'
-NOTE='\033[0;34m'
-RESET='\033[0;39m'
-
-echo -e "$NOTE Welcome to Reshakk's Ubuntu-server auto installer script! $RESET"
+echo "Welcome to Reshakk's Ubuntu-server auto installer script!"
 echo
 
 
 #Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
-        echo -e "$ERROR Please run as root $RESET"
+        echo "Please run as root"
         exit
 fi
 
@@ -33,10 +26,10 @@ fi
 read -p "Do you want to set up a new user and 'sshd_config'? (y/n) " sshdu
 sshdu=${sshdu:-n}  # Default to 'n' if no input is given
 
-read -p "Do you want to set up swap on 2G? (y/n) " swapu
+read -p "Do you want to set up swap on 2G? (y/n, default: y) " swapu
 swapu=${swapu:-n} 
 
-read -p "Do you want to set knockd on server? (y/n) " knockdu
+read -p "Do you want to set knockd on server? (y/n, default: y) " knockdu
 knockdu=${knockdu:-n}
 
 
@@ -44,7 +37,7 @@ knockdu=${knockdu:-n}
 execute_script() {
     local script_url=$1
     if ! bash <(curl -sL "$script_url"); then
-        echo "$ERROR Failed to execute script: $script_url $RESET"
+        echo "Failed to execute script: $script_url"
     fi
 }
 
