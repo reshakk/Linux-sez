3e483e1c3972f3508bb3f1b3f10352f32ebe7f23
reshakk <140834027+reshakk@users.noreply.github.com>
2024-12-27 17:01:46 +0300

    Update main-script.sh

--- main-script.sh
+++ main-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+#set -e
 
 #Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
@@ -32,27 +32,13 @@ function main_menu() {
 			break
 		fi
 		case $selection in
-			1)
-				list_users_menu
-				;;
-			2)
-				add_user
-				;;
-			3)
-				swap_menu
-				;;
-			4)
-				knockd_menu
-				;;
-			5)
-				ssh_menu
-				;;
-			6)
-				docker_menu
-				;;
-			7)
-				nextcloud_menu
-				;;
+			1) list_users_menu;;
+			2) add_user;;
+			3) swap_menu;;
+			4) knockd_menu;;
+			5) ssh_menu;;
+			6) docker_menu;;
+			7) nextcloud_menu;;
 		esac
 	done	
 }
@@ -97,6 +83,7 @@ function add_user() {
 		if [[ $? -ne 0 ]]; then
 			break
 		fi
+		
 		view_user_menu "${username}"
 	done
 }
@@ -127,94 +114,94 @@ function view_user_menu() {
 		fi
 		user_config=$(whiptail --title "Change $username Details" \
 		--menu "$MENU" $HEIGHT $WIDTH $CHOICE_HEIGHT \
-            "1" "Change Password" \
-            "2" "Change Name" \
-            "3" "Change UID" \
-            "4" "Change GID" \
-            "5" "Change Groups" \
-            "6" "Change Shell" \
-	    "7" "Change Home Directory" \
-            "8" "Exit" 3>&1 1>&2 2>&3)
-	case $user_config in
-            1)
-                new_password=$(whiptail --passwordbox "Enter new password for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
-                echo "$username:$new_password" | chpasswd
-                message_box "Successfully" "Password changed successfully!"
-                ;;
-            2)
-                new_name=$(whiptail --inputbox "Enter new name for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
-                usermod -l "$new_name" "$username"
-                message_box "Successfully" "Name changed successfully to '$new_name' !"
-                ;;
-            3)
-		current_uid=$(awk -F: -v user="$username" '$1 == user {print $3}' /etc/passwd)
-		message_box "Current UID:" " '$current_uid' "
-                new_uid=$(whiptail --inputbox "Enter new UID for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
-                usermod -u "$new_uid" "$username"
-                message_box "Successfully" "UID changed successfully to '$new_uid' !"
-                ;;
-            4)
-		current_guid=$(awk -F: -v user="$username" '$1 == user {print $4}' /etc/passwd)
-		message_box "Current GUID:" " '$current_guid' "
-                new_gid=$(whiptail --inputbox "Enter new GID for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
-                usermod -g "$new_gid" "$username"
-                message_box "Successfully" "GID changed successfully to '$new_gid' !"
-                ;;
-            5)
-		current_groups=$(groups $username)
-                message_box "Current Groups:" " $current_groups "
-		new_groups=$(whiptail --inputbox "Enter new groups for $username (comma-separated):" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
-                usermod -G "$new_groups" "$username"
-                message_box "Successfully" "New group added successfully!"
-                ;;
-            6)
-		current_shell=$(awk -F: -v user="$username" '$1 == user {print $7}' /etc/passwd)
-		message_box "Current Shell:" " $current_shell "
-                new_shell=$(whiptail --inputbox "Enter new shell for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
-                usermod -s "$new_shell" "$username"
-                message_box "Successfully" "Shell changed successfully to '$new_shell' !"
-                ;;
-	    7)
-		current_directory=$(awk -F: -v user="$username" '$1 == user {print $6}' /etc/passwd)
-		message_box "Current Home Directory:" " '$current_directory' "
-                new_directory=$(whiptail --inputbox "Enter new home directory for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
-                usermod -d "$new_directory" "$username"
-		mv "$current_directory/*" "$new_directory"
-		#rm -rf "$current_directory"
-                message_box "Successfully" "Home directory changed successfully to '$new_directory' !"
-                ;;
-	    8)
-                break
-                ;;
-	esac
+            	"1" "Change Password" \
+            	"2" "Change Name" \
+            	"3" "Change UID" \
+            	"4" "Change GID" \
+            	"5" "Change Groups" \
+            	"6" "Change Shell" \
+	    	"7" "Change Home Directory" \
+            	"8" "Exit" 3>&1 1>&2 2>&3)
+		if [[ $? -ne 0 ]]; then
+			break
+		fi
+		case $user_config in
+			1)
+                	new_password=$(whiptail --passwordbox "Enter new password for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
+                	echo "$username:$new_password" | chpasswd
+                	message_box "Successfully" "Password changed successfully!"
+                	;;
+			2)
+                	new_name=$(whiptail --inputbox "Enter new name for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
+                	usermod -l "$new_name" "$username"
+			mkdir /home/$new_name
+			chown $new_name:$username /home/$new_name
+			usermod -d /home/$new_name "$new_name"
+                	message_box "Successfully" "Name changed successfully to '$new_name' !"
+                	;;
+            		3)
+			current_uid=$(awk -F: -v user="$username" '$1 == user {print $3}' /etc/passwd)
+			message_box "Current UID:" " '$current_uid' "
+                	new_uid=$(whiptail --inputbox "Enter new UID for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
+                	usermod -u "$new_uid" "$username"
+                	message_box "Successfully" "UID changed successfully to '$new_uid' !"
+                	;;
+            		4)
+			current_guid=$(awk -F: -v user="$username" '$1 == user {print $4}' /etc/passwd)
+			message_box "Current GUID:" " '$current_guid' "
+                	new_gid=$(whiptail --inputbox "Enter new GID for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
+                	usermod -g "$new_gid" "$username"
+                	message_box "Successfully" "GID changed successfully to '$new_gid' !"
+                	;;
+            		5)
+			current_groups=$(groups $username)
+                	message_box "Current Groups:" " $current_groups "
+			new_groups=$(whiptail --inputbox "Enter new groups for $username (comma-separated):" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
+                	usermod -G "$new_groups" "$username"
+                	message_box "Successfully" "New group added successfully!"
+                	;;
+            		6)
+			current_shell=$(awk -F: -v user="$username" '$1 == user {print $7}' /etc/passwd)
+			message_box "Current Shell:" " $current_shell "
+                	new_shell=$(whiptail --inputbox "Enter new shell for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
+                	usermod -s "$new_shell" "$username"
+                	message_box "Successfully" "Shell changed successfully to '$new_shell' !"
+                	;;
+	    		7)
+			current_directory=$(awk -F: -v user="$username" '$1 == user {print $6}' /etc/passwd)
+			message_box "Current Home Directory:" " '$current_directory' "
+                	new_directory=$(whiptail --inputbox "Enter new home directory for $username:" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
+                	usermod -d "$new_directory" "$username"
+			mv "$current_directory/*" "$new_directory"
+			#rm -rf "$current_directory"
+                	message_box "Successfully" "Home directory changed successfully to '$new_directory' !"
+                	;;
+	    		8)break;;
+		esac
 	done
 }
 
 function list_users_menu() {
-  local title=$1
-  local options
-  local selection
+	local title=$1
+	local optionslocal selection
+	
+	options=$(awk -F: '$3 > 999 && $3 < 65534 {print $1, $1}' /etc/passwd)
 
-  # Create options in the format "username username"
-  options=$(awk -F: '$3 > 999 && $3 < 65534 {print $1, $1}' /etc/passwd)
+  	if [[ -z "$options" ]]; then
+    		message_box "Error" "No User Found."
+    		return
+  	fi
 
-  # Check if options are empty
-  if [[ -z "$options" ]]; then
-    message_box "Error" "No User Found."
-    return
-  fi
+  	selection=$(whiptail --clear --noitem --backtitle "$BACKTITLE" --title "$title" \
+    	--menu "Select the user" $HEIGHT $WIDTH $CHOICE_HEIGHT $options \
+    	--ok-button "Select" --cancel-button "Return" \
+    	3>&1 1>&2 2>&3)
+	if [[ $? -ne 0 ]]; then
+    		return
+  	fi
 
-  selection=$(whiptail --clear --noitem --backtitle "$BACKTITLE" --title "$title" \
-    --menu "Select the user" $HEIGHT $WIDTH $CHOICE_HEIGHT $options \
-    --ok-button "Select" --cancel-button "Return" \
-    3>&1 1>&2 2>&3)
-
-  if [[ $? -ne 0 ]]; then
-    return
-  fi
-
-  #echo "${selection}"
-  view_user_menu "${selection}"
+  	#echo "${selection}"
+  	view_user_menu "${selection}"
 }
 
 function swap_menu() {
@@ -226,16 +213,13 @@ function swap_menu() {
 		    "2" "Disable Swap" \
 		    "3" "Exit" \
 		    3>&1 1>&2 2>&3) 
+		if [[ $? -ne 0 ]]; then
+    			break
+  		fi
 		case $options in
-			1)
-				bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/swap.sh")
-				;;
-			2)
-				bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/stop-swap.sh")
-				;;
-			3)
-				break
-				;;
+			1) bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/swap.sh");;
+			2) bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/stop-swap.sh");;
+			3) break;;
 		esac
 	done
 }
@@ -252,10 +236,11 @@ function knockd_menu() {
 		    "4" "Delete Knockd" \
 		    "5" "Exit" \
 		    3>&1 1>&2 2>&3) 
+		if [[ $? -ne 0 ]]; then
+    			break
+  		fi
 		case $options in
-			1)
-				bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/start-knock.sh")
-				;;
+			1) bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/start-knock.sh");;
 			2)
 				new_port=$(whiptail --inputbox "Enter new port for port-knocking (e.g: 7000,8000,9000)" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
 				sed -i "s/sequence = .*/sequence = $new_port/" /etc/knockd.conf
@@ -265,18 +250,14 @@ function knockd_menu() {
 					message_box "Failed" "Failed to restart knockd"
 				fi
 				;;
-			3)	
-				bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/stop-knock.sh")
-				;;
+			3) bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/stop-knock.sh");;
 			4)
 				apt remove knockd -y
 				rm -f /etc/knockd.conf 
 				rm -f /etc/default/knockd 
 				rm -f /etc/systemd/system/knockd.service
 				;;
-			5)
-				break
-				;;
+			5) break;;
 		esac
 	done
 }
@@ -294,6 +275,9 @@ function ssh_menu() {
 		    "2" "Root-Login and Allow User" \
 		    "3" "Exit" \
 		    3>&1 1>&2 2>&3) 
+		if [[ $? -ne 0 ]]; then
+    			break
+  		fi
 		case $options in
 			1)
 				new_port=$(whiptail --inputbox "Enter new ssh-port" $HEIGHT $WIDTH 3>&1 1>&2 2>&3)
@@ -340,6 +324,9 @@ function docker_menu() {
 		    "3" "Install Passky" \
 		    "4" "Exit" \
 		    3>&1 1>&2 2>&3) 
+		if [[ $? -ne 0 ]]; then
+    			break
+  		fi
 		case $options in
 			1)
 				bash <(curl -sL "https://raw.githubusercontent.com/reshakk/Server-auto/master/script/docker-in.sh")
@@ -407,6 +394,9 @@ function nextcloud_menu() {
 		    "4" "Change Trusted Domains" \
 		    "5" "Exit" \
 		    3>&1 1>&2 2>&3) 
+		if [[ $? -ne 0 ]]; then
+    			break
+  		fi
 		case $options in
 			1)
 				install_nextcloud
