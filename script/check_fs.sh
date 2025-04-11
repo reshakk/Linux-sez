370c843871ad328bb8f71bc8cce0fb7c109a856e
reshakk <140834027+reshakk@users.noreply.github.com>
2025-04-11 06:11:27 +0300

    Update check_fs.sh

--- script/check_fs.sh
+++ script/check_fs.sh
@@ -1,7 +1,7 @@
 # Usage: ./check_fs.sh UUID
 
 if findfs "UUID=$1" >/dev/null; then 
-echo "$1 connected."
+    echo "$1 connected."
 else
-echo "$1 not connected."
+    echo "$1 not connected."
 fi
