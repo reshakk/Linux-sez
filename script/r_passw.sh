8b010f7d7514102791e3c1f3e6964e5dde3b36c9
reshakk <140834027+reshakk@users.noreply.github.com>
2025-07-10 00:27:47 +0300

    Update r_passw.sh

--- script/r_passw.sh
+++ script/r_passw.sh
@@ -3,14 +3,17 @@
 # First argument is the length of the password
 
 if [ $1 ]; then
-	length=$1
+        length=$1
 else
-	length=12
+        length=12
 fi
 _hash=$(python3 -c "
 import os,base64
 exec('print(base64.b64encode(os.urandom(64))[:${length}].decode(\'utf-8\'))')
 ")
-echo $_hash | xclip -selection clipboard
-echo "new password copied to the system clipboard"
-
+if which xclip >/dev/null 2>&1; then 
+        echo $_hash | xclip -selection clipboard
+        echo "new password copied to the system clipboard"
+else
+        echo $_hash
+fi
